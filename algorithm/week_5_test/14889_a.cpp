5961df8c72703e2012898acc7c8faac2d150739b
LeeDaeGyu <ttap0704@naver.com>
2023-05-04 00:27:22 +0900

    12100 복습 필요

--- algorithm/week_5_test/14889_a.cpp
+++ algorithm/week_5_test/14889_a.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 const int INF = 1e9;
-int N, a[21][21], mn = INF;
+int N, a[24][24], mn = INF;
 
 int main () {
   ios_base::sync_with_stdio(false);
@@ -10,27 +10,25 @@ int main () {
   cout.tie(NULL);
 
   cin >> N;
-  for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= N; j++) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       cin >> a[i][j];
     }
   }
 
-  for (int i = 1; i < (N * N); i++) {
-    int cnt = 0;
+  for (int i = 1; i < (1 << N); i++) {
     vector<int> v, v2;
-    for (int j = 0; j < N; j++) {
+    if (__builtin_popcount(i) != N / 2) continue;
+    for (int j = 0; j < N; j++) { 
       if (i & (1 << j)) {
-        cnt++;
-        v.push_back(j + 1);
-      } else v2.push_back(j + 1);
+        v.push_back(j);
+      } else v2.push_back(j);
     }
-    if (cnt != N / 2) continue;
 
     int sum1 = 0;
     int sum2 = 0;
-    for (int j = 0; j < v.size(); j++) {
-      for (int k = 0; k < v.size(); k++) {
+    for (int j = 0; j < N / 2; j++) {
+      for (int k = 0; k < N / 2; k++) {
         if (j != k) {
           sum1 += a[v[j]][v[k]];
           sum2 += a[v2[j]][v2[k]];
@@ -39,19 +37,6 @@ int main () {
     }
 
     mn = min(mn, abs(sum1 - sum2));
-    if (mn == abs(sum1 - sum2)) {
-      cout << "mn : " << mn << '\n';
-      cout << "v : ";
-      for (int j : v) {
-        cout << j << ' ';
-      }
-      cout << '\n';
-      cout << "v2 : ";
-      for (int j : v2) {
-        cout << j << ' ';
-      }
-      cout << '\n';
-    }
   }
 
   cout << mn << '\n';
